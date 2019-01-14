2c14c1f1464674c5e43a360ad9417812059709c5
marco.vuillermoz <marco.vuillermoz@connect-group.com>
2019-01-14 16:06:49 +0000

    commit

--- src/app/components/Pose.js
+++ src/app/components/Pose.js
@@ -1,12 +1,33 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import Posed from 'react-pose';
+
+const styles = {
+    width: '100px',
+    height: '100px',
+    background: '#ff1c68',
+    'transform-origin': '50% 50%'
+};
+
+const Box = Posed.div({
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+});
 
 export class Pose extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = { isVisible: true };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({ isVisible: !this.state.isVisible });
+        }, 1000);
     }
 
     render() {
+        const { isVisible } = this.state;
         return (
             <div>
                 <div className="container">
@@ -18,21 +39,12 @@ export class Pose extends React.Component {
 
                     <div className="row">
                         <div className="s8 offset-s2 center-align">
-                            <div className="card deep-purple z-depth-2">
-                                <div className="card-content white-text">
-                                    <span className="card-title">Awesome Animations!</span>
-                                    <p>CSS Animations are pretty cool. But combined with ReactJS ... &lt;3</p>
-                                </div>
-                                <div className="card-action">
-                                    card
-                                </div>
-                            </div>
+                            <Box style={{...styles}} pose={isVisible ? 'visible' : 'hidden'} />;
                         </div>
                     </div>
                 </div>
             </div>
-
-        );
+        )
     }
 }
 
